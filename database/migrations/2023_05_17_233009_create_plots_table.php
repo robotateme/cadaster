0a2765009412362fd3a355790d5f545eccf2cec2
robotateme <jarwork884@gmail.com>
2023-05-18 02:42:19 +0300

    Plot Entity and migration

---
+++ database/migrations/2023_05_17_233009_create_plots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plots', function (Blueprint $table) {
+            $table->string('cn')->primary();
+            $table->text('address');
+            $table->decimal('price');
+            $table->decimal('area');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plots');
+    }
+}
