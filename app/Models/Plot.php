c6ad8c0dab1f37276e5655bf885d57890bea385d
robotateme <jarwork884@gmail.com>
2023-05-18 02:51:48 +0300

    Plot Entity and migration

--- app/Models/Plot.php
+++ app/Models/Plot.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
+use Database\Factories\PlotFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Plot
+ *
+ * @property string $cn
+ * @property string $address
+ * @property string $price
+ * @property string $area
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static PlotFactory factory(...$parameters)
+ * @method static Builder|Plot newModelQuery()
+ * @method static Builder|Plot newQuery()
+ * @method static Builder|Plot query()
+ * @method static Builder|Plot whereAddress($value)
+ * @method static Builder|Plot whereArea($value)
+ * @method static Builder|Plot whereCn($value)
+ * @method static Builder|Plot whereCreatedAt($value)
+ * @method static Builder|Plot wherePrice($value)
+ * @method static Builder|Plot whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Plot extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'cn';
+
+    public $incrementing = false;
 }
