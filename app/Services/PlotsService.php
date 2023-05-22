824a94a2614492c637a560e7df67e3c50e0cfe4c
robotateme <jarwork884@gmail.com>
2023-05-22 07:53:02 +0300

    frontend

--- app/Services/PlotsService.php
+++ app/Services/PlotsService.php
@@ -2,8 +2,8 @@
 
 namespace App\Services;
 
-use App\DTOs\PlotsFilterDto;
 use App\DTOs\ApiPlotsListDto;
+use App\DTOs\PlotsFilterDto;
 use App\Models\Plot;
 use App\Repositories\Contracts\PlotsRepositoryInterface;
 use App\Repositories\PlotsRepository;
@@ -35,7 +35,7 @@ class PlotsService implements Contracts\PlotsServiceInterface
             /** @var Plot $plot */
             $plot = $plots->firstWhere('cadastral_number', '=', $plotCn);
             if (!is_null($plot)) {
-                if ($plot->updated_at->addHour() > Carbon::now()) {
+                if ($plot->updated_at->addHour() >= Carbon::now()) {
                     unset($plotsFilterData->cadastral_numbers[$k]);
                     $plotsResult[] = $plot->only($plot->getFillable());
                 }
@@ -43,7 +43,7 @@ class PlotsService implements Contracts\PlotsServiceInterface
         }
 
         if (!empty($plotsFilterData->cadastral_numbers)) {
-            $response = Http::acceptJson()->post('https://api.pkk.bigland.ru/test/plots',
+                $response = Http::acceptJson()->post('https://api.pkk.bigland.ru/test/plots',
                 [
                     'collection' => [
                         'plots' => $plotsFilterData->cadastral_numbers
@@ -51,9 +51,11 @@ class PlotsService implements Contracts\PlotsServiceInterface
                 ]
             );
 
-            $plotsData = new ApiPlotsListDto(plots: $response->json());
-            $this->plotsRepository->syncPlots($plotsData);
-            $plotsResult = array_merge($plotsResult, $plotsData->plots->toArray());
+            if ($response->ok()) {
+                $plotsData = new ApiPlotsListDto(plots: $response->json());
+                $this->plotsRepository->syncPlots($plotsData);
+                $plotsResult = array_merge($plotsResult, $plotsData->plots->toArray());
+            }
         }
 
         return $plotsResult;
